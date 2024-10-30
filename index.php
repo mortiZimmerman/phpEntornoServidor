c3fe80354de655dab4719396a192b1484f16906d
mortiZimmerman <mauricionicolasortiz@gmail.com>
2024-10-30 14:32:39 +0100

    base php

--- index.php
+++ index.php
@@ -8,12 +8,29 @@
     <title>Document</title>
 </head>
 <body>
+   Gestor de viajes MNO
+   <form action="http://localhost/actividadEvaluable1/php.php" method="GET">
+         <?php
+         
+         
+        echo "Ingrese su nombre de usuario:  <input type='text' name='nombreUsuario' required><br>
+        <label for='Paises'>Seleccione el país deseado:</label>
+        <select name='Paises' required>";
+        
+        $paises = array("España","Alemania","Portugal","Francia","Noruega","Suecia","Italia","Grecia","Armenia","Rumania","Suiza","Albania","Andorra");
+        sort($paises);
+        foreach($paises as $p){
+            $pais = $p;
+            echo "<option value='paises'>$p</option>";
+        }
+        //(^[A-Z])
+        ?>
+         </select>
+         <br>
+        <input type="submit" value="Enviar"><br>
+
+   </form>
    
-   
-   Hola mundo
-   
-   <?php
-    ?>
 </body>
 </html>
 

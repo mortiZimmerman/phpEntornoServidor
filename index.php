75bb4492be66d9b49fed3fb626066402d9edefde
mortiZimmerman <mauricionicolasortiz@gmail.com>
2024-11-04 13:05:31 +0100

    regular expresion added and index array fixed

--- index.php
+++ index.php
@@ -11,19 +11,24 @@
    Gestor de viajes MNO
    <form action="http://localhost/actividadEvaluable1/php.php" method="GET">
          <?php
+
          
-         
-        echo "Ingrese su nombre de usuario:  <input type='text' name='nombreUsuario' required><br>
+        echo "Ingrese su nombre de usuario:  
+        <input type='text' name='nombreUsuario' required><br>
         <label for='Paises'>Seleccione el país deseado:</label>
         <select name='Paises' required>";
+
         
+        //Aquí estan el array de los paises
         $paises = array("España","Alemania","Portugal","Francia","Noruega","Suecia","Italia","Grecia","Armenia","Rumania","Suiza","Albania","Andorra");
+        //con esto podemos ordenar los paises en orden alfabetico
         sort($paises);
+        //aqui imprimimos por pantalla todos los paises del array en el formulario
         foreach($paises as $p){
             $pais = $p;
             echo "<option value='paises'>$p</option>";
         }
-        //(^[A-Z])
+       
         ?>
          </select>
          <br>
--- php.php
+++ php.php
@@ -1,4 +1,11 @@
 <?php 
+//expresion regular
+$expresionRegular = "/^{[A-Z][a-zA-Z]}\s[a-zA-Z]*$/";
 
-
+$nombreUsuario = $_GET['nombreUsuario'];
+if(preg_match($expresionRegular,$nombreUsuario)){
+    echo "nombre de usuario valido";
+}else{
+    echo "El nombre de usuario no es valido";
+}
 ?>
